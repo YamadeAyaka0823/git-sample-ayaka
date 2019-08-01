b1ebc5036ca4cd31f96da17a0db7f9dbc39f7557
yamadeayaka <ayaka.yamade@rakus-partners.co.jp>
2019-08-01 15:09:26 +0900

    Carクラスを作成しました

---
+++ src/main/java/com/example/controller/Car.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+public class Car {
+	
+	private Integer speed;
+	private String bodyColor;
+	
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public String getBodyColor() {
+		return bodyColor;
+	}
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+
+}
